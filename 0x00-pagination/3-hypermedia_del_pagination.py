7f172c6223d090d503a71ad66eb13ecd1f3a66db
Mohammed Elgaily <mbajily@gmail.com>
2024-11-17 17:47:35 +0000

    0x00. Pagination

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        Return a dictionary with pagination data that's resilient to deletions.
+
+        Args:
+            index (int): The starting index of the page
+            page_size (int): The size of the page
+
+        Returns:
+            Dict: Dictionary containing pagination data
+        """
+        dataset = self.indexed_dataset()
+        assert index is not None and index >= 0 and index < len(self.dataset())
+
+        data = []
+        next_index = index
+        for _ in range(page_size):
+            while next_index < len(self.dataset()) and next_index not in dataset:
+                next_index += 1
+            if next_index < len(self.dataset()):
+                data.append(dataset[next_index])
+                next_index += 1
+            else:
+                break
+
+        return {
+            'index': index,
+            'next_index': next_index,
+            'page_size': len(data),
+            'data': data
+        }
